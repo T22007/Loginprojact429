ab9294abafe1ca896f30826b4569c0abd252a0ba
T22 <78694408+T22007@users.noreply.github.com>
2023-09-04 14:29:54 -0700

    Update logininterface.py

--- logininterface.py
+++ logininterface.py
@@ -1,1 +1,13 @@
-
+class user:
+    username = ""
+    userpass = ""
+javad = user
+javad.username = "T22"
+javad.userpass = "123456"
+def identify (username1,password1):
+# inja etelaat ro tatabogh mide
+    if username1 == "user":
+        if password1 == javad.userpass:
+            print("Logged in")
+    else:
+        print("incorrect")

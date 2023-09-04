d54f1ff2cca7c997a05c385269e0394e5b093147
samin6678 <144063104+samin6678@users.noreply.github.com>
2023-09-05 01:26:29 +0330

    Update sitebrowser.py

--- sitebrowser.py
+++ sitebrowser.py
@@ -1,1 +1,9 @@
-#backend
+from tkinter import Tk, Button,Label,Listbox
+import webbrowser
+win=Tk()
+win.title("web browser")
+win.geometry('800x450')
+def  open_browser():
+    webbrowser.open_new_tab('http:/www.python.org')
+Button(win,text='web',command=open_browser,bg='blue',width=24,height=6,font=('arial',17)).grid(row=3,column=3)   
+win.mainloop() 
